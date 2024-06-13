173d7ec4e18bb20472b37b0f845b4fb71da632d0
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2024-06-12 23:12:08 -0300

    Create scheduler.

---
+++ quotation/src/main/java/com/lucasmottadev/service/QuotationService.java
@@ -0,0 +1,9 @@
+package com.lucasmottadev.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class QuotationService {
+    public void getCurrencyPrice() {
+    }
+}
